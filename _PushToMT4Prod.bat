6f7f0b2bbe0b597389b43979091d6d96c8f77073
surapoom <45107078+surapoom@users.noreply.github.com>
2019-01-13 00:44:18 +0700

    Adapt to local

--- _PushToMT4Prod.bat
+++ _PushToMT4Prod.bat
@@ -4,16 +4,14 @@ rem Use when you need to publish all files to All Terminals
 @echo off
 setlocal enabledelayedexpansion
 
-set SOURCE_DIR="C:\Users\fxtrams\Documents\000_TradingRepo\DataWriter"
-set DEST_DIR1="C:\Program Files (x86)\FxPro - Terminal1\MQL4\Experts\01_DataWriter"
-set DEST_DIR2="C:\Program Files (x86)\FxPro - Terminal2\MQL4\Experts\01_DataWriter"
-set DEST_DIR3="C:\Program Files (x86)\FxPro - Terminal3\MQL4\Experts\01_DataWriter"
-set DEST_DIR4="C:\Program Files (x86)\FxPro - Terminal4\MQL4\Experts\01_DataWriter"
+set SOURCE_DIR="C:\LazyTrading\GitHub\DataWriter"
+set DEST_DIR1="C:\Program Files (x86)\ICMarkets MT4 Terminal1\MQL4\Experts\01_DataWriter"
+set DEST_DIR2="C:\Program Files (x86)\ICMarkets MT4 Terminal2\MQL4\Experts\01_DataWriter"
+set DEST_DIR3="C:\Program Files (x86)\ICMarkets MT4 Terminal3\MQL4\Experts\01_DataWriter"
+set DEST_DIR4="C:\Program Files (x86)\ICMarkets MT4 Terminal4\MQL4\Experts\01_DataWriter"
 
 ROBOCOPY %SOURCE_DIR% %DEST_DIR1% *.mq4
 ROBOCOPY %SOURCE_DIR% %DEST_DIR2% *.mq4
 ROBOCOPY %SOURCE_DIR% %DEST_DIR3% *.mq4
 ROBOCOPY %SOURCE_DIR% %DEST_DIR4% *.mq4
-
-
 
